59724703cfd6af65819cefedc7062aa1a565d7d7
livrth <lyvrth@gmail.com>
2021-03-28 11:59:41 +0800

    补充逆序对的树状数组求法

--- ds/tree_array.cc
+++ ds/tree_array.cc
@@ -139,3 +139,51 @@ int main() {
     for (int i = 1; i <= n; i++) printf("%d\n", ans[i]);
     return 0;
 }
+/*树状数组求逆序对
+整数范围 显然要先离散化才能在树状数组add
+由于会出现相同的元素，而且std::sort是不稳定排序
+离散化之后的数是没有相同的，相同元素必须保持原来插入的顺序，不然会错
+此时要用双关键字排序 保证了插入顺序在离散化之后也是递增
+每次统计即可，比如现在遍历插入的第8个数，那么加上比当前数大的数的个数即可
+*/
+#define x first //元素的值
+#define y second //第几个加入的
+typedef pair<int, int> pii;
+const int N = 100003;
+pii a[N]; //所有的点 双关键字排序
+int tr[N], rk[N]; //rk是离散化映射
+int n;
+//双关键字排序 值相同的按照加入顺序升序
+bool cmp(pii a, pii b) {
+    if (a.x == b.x)
+        return a.y < b.y;
+    return a.x < b.x;
+}
+int lowbit(int x) {
+    return x & -x;
+}
+void add(int x, int c) {
+    for (int i = x; i <= n; i += lowbit(i)) tr[i] += c;
+}
+ll sum(int x) {
+    int res = 0;
+    for (int i = x; i; i -= lowbit(i)) res += tr[i];
+    return res;
+}
+int main() {
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i].x; //x是val值
+        a[i].y = i; //加入的顺序
+    }
+    sort(a + 1, a + 1 + n, cmp);
+    for (int i = 1; i <= n; i++)
+        rk[a[i].y] = i;
+    ll ans = 0;
+    for (int i = 1; i <= n; i++) {
+        add(rk[i], 1);
+        ans += i - sum(rk[i]);
+    }
+    cout << ans << endl;
+    return 0;
+}
