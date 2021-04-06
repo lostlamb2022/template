ad584715f852c39c97e3a39b56aefe8ccaaa9ac5
livrth <lyvrth@gmail.com>
2021-04-06 16:17:36 +0800

    更新Splay模板

--- ds/splay.cc
+++ ds/splay.cc
@@ -1,5 +1,4 @@
-/*
-给定一个长度为 n 的整数序列，初始时序列为 {1,2,…,n−1,n}。
+/*给定一个长度为 n 的整数序列，初始时序列为 {1,2,…,n−1,n}。
 序列中的位置从左到右依次标号为 1~n。
 我们用 [l,r] 来表示从位置 l 到位置 r 之间（包括两端点）的所有数字构成的子序列。
 现在要对该序列进行 m 次操作，每次操作选定一个子序列 [l,r]，并将该子序列中的所有数字进行翻转。
@@ -88,7 +87,7 @@ void output(int u) { //从根节点开始中序输出
 }
 int main() {
     scanf("%d%d", &n, &m);
-    for (int i = 0; i <= n + 1; i++) insert(i); //0,n+1是哨兵 放置RE
+    for (int i = 0; i <= n + 1; i++) insert(i); //0,n+1是哨兵 防止RE
     while (m --) { //翻转区间的操作
         int l, r;
         scanf("%d%d", &l, &r);
@@ -99,5 +98,212 @@ int main() {
         tr[tr[r].s[0]].flag ^= 1; //旋转之后r的左子树就是要旋转的区间了 更新flag即可
     }
     output(root); //输出中序遍历
+    return 0;
+}
+/*NOI2004《郁闷的出纳员》：有几种操作:
+I 命令，格式为 I_k，表示新建一个工资档案，初始工资为 k。如果某员工的初始工资低于工资下界，他将立刻离开公司。
+A 命令，格式为 A_k，表示把每位员工的工资加上 k。
+S 命令，格式为 S_k，表示把每位员工的工资扣除 k。
+F 命令，格式为 F_k，表示查询第 k 多的工资。
+对于每次询问输出，以及最后输出离职人数。
+用Splay维护所有员工工资的BST,由于是对所有区间元素增加或者减小 直接记录一个偏移量delta即可
+每次扣除工资操作就删除前缀小于 m - delta 的区间删去即可
+查询就是最基本的BST查询，维护size即可
+*/
+const int N = 100010, INF = 0x3f3f3f3f;
+int n, m, delta;
+struct Node {
+    int s[2], p, v;
+    int size;
+    void init(int _v, int _p) {
+        v = _v, p = _p;
+        size = 1;
+    }
+} tr[N];
+int root, idx;
+void pushup(int x) {
+    tr[x].size = tr[tr[x].s[0]].size + tr[tr[x].s[1]].size + 1;
+}
+void rotate(int x) {
+    int y = tr[x].p, z = tr[y].p;
+    int k = tr[y].s[1] == x;
+    tr[z].s[tr[z].s[1] == y] = x, tr[x].p = z;
+    tr[y].s[k] = tr[x].s[k ^ 1], tr[tr[x].s[k ^ 1]].p = y;
+    tr[x].s[k ^ 1] = y, tr[y].p = x;
+    pushup(y), pushup(x);
+}
+void splay(int x, int k) {
+    while (tr[x].p != k) {
+        int y = tr[x].p, z = tr[y].p;
+        if (z != k)
+            if ((tr[y].s[1] == x) ^ (tr[z].s[1] == y)) rotate(x);
+            else rotate(y);
+        rotate(x);
+    }
+    if (!k) root = x;
+}
+int insert(int v) {
+    int u = root, p = 0;
+    //插入是每个点的val满足BST 不是节点的tr数组编号
+    while (u) p = u, u = tr[u].s[v > tr[u].v];
+    u = ++idx;
+    if (p) tr[p].s[v > tr[p].v] = u;
+    tr[u].init(v, p);
+    splay(u, 0);
+    return u; //返回该点的tr数组编号
+}
+int lower_bound_(int v) { //返回val大于等于v的节点编号
+    int u = root, res;
+    while (u) {
+        if (tr[u].v >= v) res = u, u = tr[u].s[0];
+        else u = tr[u].s[1];
+    }
+    return res;
+}
+int get_k(int k) { //返回第k小的val
+    int u = root;
+    while (u) {
+        if (tr[tr[u].s[0]].size >= k) u = tr[u].s[0];
+        else if (tr[tr[u].s[0]].size + 1 ==  k) return tr[u].v;
+        else k -= tr[tr[u].s[0]].size + 1,  u = tr[u].s[1];
+    }
+    return -1; //没找到
+}
+int main() {
+    scanf("%d%d", &n, &m);
+    int L = insert(-INF), R = insert(INF); //哨兵 返回tr数组中的编号
+    int tot = 0; //insert的员工总数量
+    while (n --) {
+        char op[2];
+        int k;
+        scanf("%s%d", op, &k);
+        if (*op == 'I') {
+            if (k >= m) k -= delta, insert(k), tot ++; //新员工的工资要先减去当前的偏移量，因为偏移量已经算上了该员工之前的加减工资操作
+        }
+        else if (*op == 'A') delta += k;
+        else if (*op == 'S') {
+            delta -= k;
+            R = lower_bound_(m - delta); //R是>=m-delta的第一个数的编号
+            splay(L, 0), splay(R, L); //这里的删除怎么写都行 只要记住L的中序肯定在R左边即可
+            tr[R].s[0] = 0;
+            pushup(R), pushup(L); //删去节点之后 要先pushup下边的节点 后pushup上边的
+        }
+        else {
+            if (tr[root].size - 2 < k) puts("-1");
+            else printf("%d\n", get_k(tr[root].size - k) + delta);
+        }
+    }
+    printf("%d\n", tot - (tr[root].size - 2)); //减去哨兵 以及现存的人 就是离职的人数
+    return 0;
+}
+/*HNOI2012《永无乡》：永无乡包含 n 座岛，编号从 1 到 n ，每座岛都有自己的独一无二的重要度，按照重要度可以将这 n 座岛排名，名次用 1 到 n 来表示。
+某些岛之间由巨大的桥连接，通过桥可以从一个岛到达另一个岛。如果从岛 a 出发经过若干座（含 0 座）桥可以到达岛 b ，则称岛 a 和岛 b 是连通的。
+现在有两种操作：
+B x y 表示在岛 x 与岛 y 之间修建一座新桥。
+Q x k 表示询问当前与岛 x 连通的所有岛中第 k 重要的是哪座岛，即所有与岛 x 连通的岛中重要度排名第 k 小的岛是哪座，请你输出那个岛的编号。
+n<=100000,m<=n,q<=300000
+用DSU维护所有splay的关系,初始每一个点就是一颗splay,然后合并
+合并为启发式合并，size小的splay的所有点暴力dfs插入size大的splay中
+本质上就是把小的splay的所有点复制一份插入大的splay然后更新并查集的p[]祖宗关系
+每次查询只会查询祖宗节点所在DSU节点的root，也就是说，将小的splay插入到大的splay
+之后，查询原来小的splay中的id时，find()函数返回的root会定位到合并之后的大的splay
+*/
+const int N = 1800010;//启发式合并的数量上限是 O(nlogn)，每次合并时均新增节点，所以总节点数应为n+nlogn
+int n, m;
+struct Node {
+    int s[2], p, v, id;
+    int size;
+    void init(int _v, int _id, int _p) {
+        v = _v, id = _id, p = _p;
+        size = 1;
+    }
+} tr[N];
+int root[N], idx; //root[]记录每一颗splay的根节点
+int p[N]; //DSU的祖宗节点
+int find(int x) {
+    if (p[x] != x) p[x] = find(p[x]);
+    return p[x];
+}
+void pushup(int x) {
+    tr[x].size = tr[tr[x].s[0]].size + tr[tr[x].s[1]].size + 1;
+}
+void rotate(int x) {
+    int y = tr[x].p, z = tr[y].p;
+    int k = tr[y].s[1] == x;
+    tr[z].s[tr[z].s[1] == y] = x, tr[x].p = z;
+    tr[y].s[k] = tr[x].s[k ^ 1], tr[tr[x].s[k ^ 1]].p = y;
+    tr[x].s[k ^ 1] = y, tr[y].p = x;
+    pushup(y), pushup(x);
+}
+void splay(int x, int k, int b) { //将b集合的x旋转到k下
+    while (tr[x].p != k) {
+        int y = tr[x].p, z = tr[y].p;
+        if (z != k)
+            if ((tr[y].s[1] == x) ^ (tr[z].s[1] == y)) rotate(x);
+            else rotate(y);
+        rotate(x);
+    }
+    if (!k) root[b] = x;
+}
+void insert(int v, int id, int b) { //将id的点插入b集合
+    int u =  root[b], p = 0;
+    while (u) p = u, u = tr[u].s[v > tr[u].v];
+    u = ++idx;
+    if (p) tr[p].s[v > tr[p].v] = u;
+    tr[u].init(v, id, p);
+    splay(u, 0, b);
+}
+int get_k(int k, int b) { //返回集合b中val第k小的id
+    int u = root[b];
+    while (u) {
+        if (tr[tr[u].s[0]].size >= k) u = tr[u].s[0];
+        else if (tr[tr[u].s[0]].size + 1 == k) return tr[u].id;
+        else k -=  tr[tr[u].s[0]].size + 1, u = tr[u].s[1];
+    }
+    return -1;
+}
+void dfs(int u, int b) { //将u所在的集合所有点插入b集合
+    if (tr[u].s[0]) dfs(tr[u].s[0], b);
+    if (tr[u].s[1]) dfs(tr[u].s[1], b);
+    insert(tr[u].v, tr[u].id, b);
+}
+int main() {
+    scanf("%d%d", &n, &m);
+    for (int i = 1; i <= n; i++) {
+        p[i] = root[i] = i;
+        int v;
+        scanf("%d", &v);
+        tr[i].init(v, i, 0);
+    }
+    idx = n;
+    while (m --) {
+        int a, b;
+        scanf("%d%d", &a, &b);
+        a = find(a), b = find(b);
+        if (a != b) {
+            if (tr[root[a]].size > tr[root[b]].size) swap(a, b);
+            dfs(root[a], b);
+            p[a] = b;
+        }
+    }
+    scanf("%d", &m);
+    while (m --) {
+        char op[2];
+        int a, b;
+        scanf("%s%d%d", op, &a, &b);
+        if (*op == 'B') {
+            a = find(a), b = find(b);
+            if (a != b) {
+                if (tr[root[a]].size > tr[root[b]].size) swap(a, b);
+                dfs(root[a], b);
+                p[a] = b;
+            }
+        }
+        else {
+            a = find(a);
+            if (tr[root[a]].size < b) puts("-1");
+            else printf("%d\n", get_k(b, a));
+        }
+    }
     return 0;
 }
