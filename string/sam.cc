a169fef288d7a90e5be61262a4545b8cb203de0f
livrth <lyvrth@gmail.com>
2021-04-19 17:01:24 +0800

    更新SAM模板/添加JSOI2012《玄武密码》模板题

--- string/sam.cc
+++ string/sam.cc
@@ -1,3 +1,50 @@
+/*JSOI2012《玄武密码》：给定母串和一些短串，求每个短串和母串匹配的最大短串前缀长度,一个短串直接在母串的SAM暴力匹配同时维护长度，直到匹配不上就停止,O(N)*/
+const int N = 1e7 + 10;//1e7*int是40MB
+int n, m;
+int tot = 1, last = 1;
+char str[N];// 母串
+struct Node {
+    int len, fa;
+    int ch[4];
+} node[N * 2];
+inline int get(char c) {//字母映射数字
+    if (c == 'E') return 0;
+    if (c == 'S') return 1;
+    if (c == 'W') return 2;
+    else return 3;
+}
+void extend(int c) {
+    int p = last, np = last = ++tot;
+    node[np].len = node[p].len + 1;
+    for (; p && !node[p].ch[c]; p = node[p].fa) node[p].ch[c] = np;
+    if (!p) node[np].fa = 1;
+    else {
+        int q = node[p].ch[c];
+        if (node[q].len == node[p].len + 1) node[np].fa = q;
+        else {
+            int nq = ++tot;
+            node[nq] = node[q], node[nq].len = node[p].len + 1;
+            node[q].fa = node[np].fa = nq;
+            for (; p && node[p].ch[c] == q; p = node[p].fa)node[p].ch[c] = nq;
+        }
+    }
+}
+int main() {
+    scanf("%d%d", &n, &m);
+    scanf("%s", str);
+    for (int i = 0; str[i]; i++) extend(get(str[i]));
+    while (m --) {
+        scanf("%s", str);
+        int p = 1, res = 0;
+        for (int i = 0; str[i]; i++) {
+            int c = get(str[i]);
+            if (node[p].ch[c]) p = node[p].ch[c], res ++;
+            else break;
+        }
+        printf("%d\n", res);
+    }
+    return 0;
+}
 /*给定一个长度为n只包含小写字母的字符串S,对于所有 S 的出现次数不为 1 的子串，设其value为该子串出现的次数乘该子串的长度,求value的最大值。
 用每个状态代表的节点的endpos集合大小乘该集合的longest更新即可,注意dfs的时候，要将link边反向*/
 typedef long long ll;
